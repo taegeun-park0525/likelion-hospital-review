5af9d64130da7f12a9ea6ed1cb0ff202bb6dc47c
taegeun <djqtek0525@naver.com>
2022-12-08 14:33:43 +0900

    first commit

---
+++ src/main/java/com/review/hospital/LikelionHospitalReviewApplication.java
@@ -0,0 +1,13 @@
+package com.review.hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikelionHospitalReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikelionHospitalReviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/review/hospital/LikelionHospitalReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.review.hospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikelionHospitalReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
